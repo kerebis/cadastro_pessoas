2f09f6e4716dd7d9c31a7aa692850d5a132c7574
kerebis <cleberalbuquerqueneto@hotmail.com>
2024-11-26 18:51:38 -0300

    Update

--- pessoas/urls.py
+++ pessoas/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listar_pessoas, name='listar_pessoas'),
+    path('', views.pagina_inicial, name='pagina_inicial'),
+    path('listar/', views.listar_pessoas, name='listar_pessoas'),
     path('cadastrar/', views.cadastrar_pessoa, name='cadastrar_pessoa'),
+    path('deletar/<int:pessoa_id>/', views.deletar_pessoa, name='deletar_pessoa'),
+    path('editar/<int:pessoa_id>/', views.editar_pessoa, name='editar_pessoa'),
 ]
--- pessoas/views.py
+++ pessoas/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Pessoa
 from .forms import PessoaForm
+
+def pagina_inicial(request):
+    return render(request, 'pessoas/pagina_inicial.html')
 
 def listar_pessoas(request):
     pessoas = Pessoa.objects.all()
@@ -16,3 +19,20 @@ def cadastrar_pessoa(request):
         form = PessoaForm()
     return render(request, 'pessoas/cadastrar_pessoa.html', {'form': form})
 
+def editar_pessoa(request, pessoa_id):
+    pessoa = get_object_or_404(Pessoa, id=pessoa_id)
+    if request.method == 'POST':
+        form = PessoaForm(request.POST, instance=pessoa)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_pessoas')
+    else:
+        form = PessoaForm(instance=pessoa)
+    return render(request, 'pessoas/editar_pessoa.html', {'form': form, 'pessoa': pessoa})
+
+def deletar_pessoa(request, pessoa_id):
+    pessoa = get_object_or_404(Pessoa, id=pessoa_id)
+    if request.method == 'POST':
+        pessoa.delete()
+        return redirect('listar_pessoas')
+    return render(request, 'pessoas/deletar_pessoa.html', {'pessoa': pessoa})
